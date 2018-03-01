3b4ff2cd9102c0cf9d985ca264d77e33656881be
forgetIt <2271404280@qq.com>
2018-03-01 15:43:44 +0800

    03-01 beauty

--- 函数/nested_closure.py
+++ 函数/nested_closure.py
@@ -8,16 +8,16 @@ def multi_fun():
     返回的内层函数不要引用任何循环变量，或者后续会发生变化的变量
     """
     fs = []
-    for i in xrange(4):
+    for i in range(4):
         def f(x):
             return i * x  # 返回的内层函数引用了变量i，但它并非立刻执行
         fs.append(f)
     return fs
 
-f0, f1, f2, f3 = multi_fun()  # 等到函数都返回时，它们所引用的变量都i已经变了
-print f0(2), f1(2), f2(2), f3(2)
+f1, f2, f3, f4 = multi_fun()  # 等到函数都返回时，它们所引用的变量都i已经变了
+print(f1(2), f2(2), f3(2), f4(2))
 # def multi_fun(): return [lambda x: i * x for i in xrange(4)]
-print[m(2) for m in multi_fun()]
+print([m(2) for m in multi_fun()])
 print("-" * 20)
 
 
@@ -37,8 +37,8 @@ def multi_fun():
             return j * x
         return g
     fs = []
-    for i in xrange(4):
+    for i in range(4):
         fs.append(f(i))
     return fs
 f1, f2, f3, f4 = multi_fun()
-print f1(2), f2(2), f3(2), f4(2)
+print(f1(2), f2(2), f3(2), f4(2))
