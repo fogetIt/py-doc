660eac9917aca633a887aa97b8119b1c3df9728f
forgetIt <2271404280@qq.com>
2018-03-08 18:49:30 +0800

    03-07 异常处理

--- 异常处理/demo_exception.py
+++ 异常处理/demo_exception.py
@@ -1,45 +1,34 @@
 # -*- coding: utf-8 -*-
 # @Date:   2016-07-12 13:06:08
 # @Last Modified time: 2017-11-21 11:05:41
-#
-"""
-except [exception1[,exception2...] as obj]
-python3 不再支持 except Exception, e，必须使用 as
-捕获异常类型（exception1[,exception2...]）的详细原因，并放在对象（obj）中
-"""
-import os
-filepath = os.path.abspath('.')
-print filepath
 
 
 """
 异常处理语句嵌套
+    外层 try 子句代码引发异常，程序直接跳转到对应的外层 except 子句
+    内部的 try 子句不会被执行
+多个并列的 except 语句
+    子类异常写在前面，父类写在后面，否则捕获不到子类异常
 """
 try:
-    f = open(filepath, 'r')
-    """
-    外层try子句代码引发异常
-    程序直接跳转到外层try对应的except子句
-    内部的try子句不会被执行
-    """
-    try:
-        print 'ok'
-    except:
-        print 'no'
-except ValueError as e:
-    print 'ValueError:', e
-except IOError, e:
-    print 'IOError', e
-    try:
-        filename = os.path.join(filepath, 'demo_exception.py')
-        f = open(filename, 'r')
-        f.close()
-        print 'ok'
-    except Exception, e:
-        print 'inner:', e
-except Exception, e:
-    """
-    多个并列的except语句，子类异常写在前面，父类写在后面
-    """
-    print 'outer:', e
-print
+    print( 2 / 0)
+    try: print('ok')
+    except: print('no')
+except ZeroDivisionError as e:
+    print('ZeroDivisionError', e)
+    try: print(2 / 1)
+    except Exception as e: print('inner:', e)
+except Exception as e:
+    print('outer:', e)
+
+
+try:
+    print( 2 / 0)
+    try: print('ok')
+    except: print('no')
+except ZeroDivisionError as e:
+    print('ZeroDivisionError', e)
+    try: print(2 / 1)
+    except Exception as e: print('inner:', e)
+except Exception as e:
+    print('outer:', e)
