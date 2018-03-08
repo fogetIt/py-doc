eb6dd05c66c99619fd5ee35992ef9355b99847d6
forgetIt <2271404280@qq.com>
2018-03-08 10:17:21 +0800

    03-06 内存管理 beauty

--- 内存管理/_gc.py
+++ 内存管理/_gc.py
@@ -4,61 +4,45 @@
 import gc
 from sys import getrefcount
 gc.set_debug(gc.DEBUG_LEAK)
-gc.disable()
-print gc.get_threshold()
-gc.set_threshold()
-gc.get_count()
-"""
-"""
-x = 10
-print gc.collect()
-del x
-print gc.collect()
-
-"""
-引用环(reference cycle)
-"""
-# 两个对象可能相互引用，从而构成引用环
-a = []
-b = [a]
-a.append(b)
-print a, getrefcount(a)
-print b, getrefcount(b)
-
-# 一个对象自己引用自己，也能构成引用环
-c = []
-c.append(c)
-print c, getrefcount(c)
-print("-" * 20)
-
-
-print gc.garbage
-print gc.collect()
-del a
-del b
-
-print("-" * 20)
-print gc.garbage
-print gc.collect()
-
-# print gc.get_count()
-
-
-class ClassA(object):
-    pass
+# gc.disable()
+# print gc.get_threshold()
+# print(gc.set_threshold())
+# gc.set_threshold()
+# gc.get_count()
+# """
+# """
+# x = 10
+# print gc.collect()
+# del x
+# print gc.collect()
+#
+# """
+# 引用环(reference cycle)
+# """
+# # 两个对象可能相互引用，从而构成引用环
+# a = []
+# b = [a]
+# a.append(b)
+# print a, getrefcount(a)
+# print b, getrefcount(b)
+#
+# # 一个对象自己引用自己，也能构成引用环
+# c = []
+# c.append(c)
+# print c, getrefcount(c)
+# print("-" * 20)
+#
+#
+# print gc.garbage
+# print gc.collect()
+# del a
+# del b
+#
+# print("-" * 20)
+# print gc.garbage
+# print gc.collect()
+#
+# # print gc.get_count()
 
 
-def f2():
-    """
-    循环引用导致内存泄露
-    """
-    while True:
-        c1 = ClassA()
-        c2 = ClassA()
-        c1.t = c2
-        c2.t = c1  # 两块内存的引用计数变成2
-        del c1  # del c1后，内存1的对象的引用计数变为1
-        del c2
 
-# 执行f2()，进程占用的内存会不断增大
-f2()
